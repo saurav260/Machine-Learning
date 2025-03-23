1bb92093e5aa8ac572a34e635454d3b8a9c388aa
Saurav Doke <sauravdoke@gmail.com>
2025-03-23 14:38:10 +0530

    array indexings

---
+++ array elements/ele.py
@@ -0,0 +1,20 @@
+import numpy as np
+array =np.array([45,56,89,58])
+print(array[3])
+
+# add elements of array
+import numpy as np 
+arr=np.array([78,25,13,14])
+print(arr[0]+arr[3])
+
+# 2d arrays
+import numpy as np
+newarr = np.array([[24,15,12,19],[78,45,12,96]])
+print(newarr[1,3])
+
+# negative indexing
+import numpy as np
+
+arr1 = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('Last element from 2nd dim: ', arr1[1, -3])
