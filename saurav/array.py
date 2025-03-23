bf691d649c9a1590d63cda78c779ece57e23afb4
Saurav Doke <sauravdoke@gmail.com>
2025-03-23 14:26:16 +0530

    arrays

---
+++ saurav/array.py
@@ -0,0 +1,29 @@
+'''import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+import numpy as np
+
+print(np.__version__)'''
+
+#import numpy as np 
+#array =np.array([26,22,31,4,1,21])
+#print(array)
+#print(type(array))
+
+# 0d Array
+''' import numpy as np 
+array =np.array(54)
+print(array) '''
+
+# 1d Array
+'''import numpy as np
+array = np.array([26,22,31,4,1,21])
+print (array)'''
+
+# 2d Array
+'''import numpy as np
+array =np.array([[26,22,31],[25,24,29]])
+print (array)'''
